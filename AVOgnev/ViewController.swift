eccb92f5678a74634e546889ad152dac6b2b1d84
Anton <baterlbee@gmail.com>
2019-09-22 14:52:43 +0300

    Initial Commit

---
+++ AVOgnev/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AVOgnev
+//
+//  Created by Anton on 22/09/2019.
+//  Copyright © 2019 Anton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
