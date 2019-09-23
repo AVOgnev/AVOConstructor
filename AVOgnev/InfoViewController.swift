f400f4198700460ab7b815bbd56502670df01054
Anton <baterlbee@gmail.com>
2019-09-23 23:11:07 +0300

    add InfoViewController

---
+++ AVOgnev/InfoViewController.swift
@@ -0,0 +1,13 @@
+//
+//  InfoViewController.swift
+//  AVOgnev
+//
+//  Created by Anton on 23/09/2019.
+//  Copyright © 2019 Anton. All rights reserved.
+//
+
+import Foundation
+
+class InfoViewController {
+    
+}
